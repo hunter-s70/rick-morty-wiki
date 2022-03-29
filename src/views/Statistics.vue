f7296f3c6d00c409186a3db0ab49a2fa7e055fcd
Sergey Bukhtoyarov <serg.buhtoyarov@gmail.com>
2022-03-29 18:33:41 +0300

    first version of LineChart added

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="statistics">
     <h1>Statistics should be here</h1>
+    <line-chart :data="[99, 71, 78, 25, 36, 92]" />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import LineChart from "@/components/LineChart.vue";
+
+export default defineComponent({
+  name: "Statistics",
+  components: {
+    LineChart,
+  },
+});
+</script>
