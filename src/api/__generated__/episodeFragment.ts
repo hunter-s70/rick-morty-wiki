a21a4bc77475d0fbbc13cbba7e06b8dcd386c1ff
Sergey Bukhtoyarov <serg.buhtoyarov@gmail.com>
2022-04-02 20:44:25 +0300

    add additional info and fragments to the character

---
+++ src/api/__generated__/episodeFragment.ts
@@ -0,0 +1,28 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL fragment: episodeFragment
+// ====================================================
+
+export interface episodeFragment {
+  __typename: "Episode";
+  /**
+   * The id of the episode.
+   */
+  id: string | null;
+  /**
+   * The name of the episode.
+   */
+  name: string | null;
+  /**
+   * The code of the episode.
+   */
+  episode: string | null;
+  /**
+   * Time at which the episode was created in the database.
+   */
+  created: string | null;
+}
---
+++ src/api/__generated__/locationFragment.ts
@@ -0,0 +1,32 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL fragment: locationFragment
+// ====================================================
+
+export interface locationFragment {
+  __typename: "Location";
+  /**
+   * The id of the location.
+   */
+  id: string | null;
+  /**
+   * The name of the location.
+   */
+  name: string | null;
+  /**
+   * The type of the location.
+   */
+  type: string | null;
+  /**
+   * The dimension in which the location is located.
+   */
+  dimension: string | null;
+  /**
+   * Time at which the location was created in the database.
+   */
+  created: string | null;
+}
