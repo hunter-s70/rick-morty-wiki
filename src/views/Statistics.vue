dc3c587980b09592dced86b7697ea33143c2b880
Sergey Bukhtoyarov <serg.buhtoyarov@gmail.com>
2022-04-02 20:30:11 +0300

    no data for statistic

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="statistics">
     <h1>Statistics should be here</h1>
+    <ol class="list">
+      <li>Statistics have no sense</li>
+      <li>
+        Could be applicable for single types: Location, Episode, Character
+      </li>
+      <li>There is not so match data</li>
+      <li>
+        Here is a simple example of D3.js line chart generation in Vue app
+      </li>
+    </ol>
     <line-chart :data="[99, 71, 78, 25, 36, 92]" />
   </div>
 </template>
@@ -16,3 +26,9 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="scss">
+.list {
+  text-align: left;
+}
+</style>
