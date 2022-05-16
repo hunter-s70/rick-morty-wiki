8976f1358f3ffb7cebf3589c9c0c9b78c7a2c7f3
hunter-s70 <serg.buhtoyarov@gmail.com>
2022-05-16 10:13:07 +0300

    fix types for application

--- src/services/apollo/possible-types.ts
+++ src/services/apollo/possible-types.ts
@@ -1,12 +1,23 @@
 // PossibleTypes is currently null for all types http://joxi.ru/MAjEXJdFdkbWq2
 // For more details check https://v4.apollo.vuejs.org/guide-composable/fragments.html#fragments-on-unions-and-interfaces
-export default function getPossibleTypes(data: any) {
-  const possibleTypes: any = {};
+import {
+  IntrospectionInterfaceType,
+  IntrospectionNamedTypeRef,
+  IntrospectionObjectType,
+} from "graphql/utilities/getIntrospectionQuery";
+import { PossibleTypesMap } from "@apollo/client/core";
 
-  data.__schema.types.forEach((supertype: any) => {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export default function getPossibleTypes(data: any): PossibleTypesMap {
+  /* eslint-enable @typescript-eslint/no-explicit-any */
+  const possibleTypes: PossibleTypesMap = {};
+
+  data.__schema.types.forEach((supertype: IntrospectionInterfaceType) => {
     if (supertype.possibleTypes) {
       possibleTypes[supertype.name] = supertype.possibleTypes.map(
-        (subtype: any) => subtype.name
+        (subtype: IntrospectionNamedTypeRef<IntrospectionObjectType>): string =>
+          subtype.name
       );
     }
   });
