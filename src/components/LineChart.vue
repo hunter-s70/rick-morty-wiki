51b82fd0ff74c06dbbeb37a6668f0b347e7f623c
hunter-s70 <serg.buhtoyarov@gmail.com>
2022-05-22 00:31:11 +0300

    add line chart axis

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,6 +1,12 @@
 <template>
   <svg class="line-chart" :viewBox="viewBox">
     <g transform="translate(0, 10)">
+      <g :transform="`translate(${padding}, 0)`">
+        <g ref="yAxis"></g>
+      </g>
+      <g :transform="`translate(0, ${height - padding})`">
+        <g ref="xAxis"></g>
+      </g>
       <path class="line-chart__line" :d="line" />
     </g>
   </svg>
@@ -28,36 +34,71 @@ export default defineComponent({
   },
   data() {
     return {
-      padding: 60,
+      line: "",
+      padding: 40,
     };
   },
   computed: {
     rangeX() {
       const width = this.width - this.padding;
-      return [0, width];
+      return [this.padding, width];
     },
     rangeY() {
       const height = this.height - this.padding;
       return [0, height];
     },
-    path() {
-      const x = d3.scaleLinear().range(this.rangeX);
-      const y = d3.scaleLinear().range(this.rangeY);
-      d3.axisLeft().scale(x);
-      d3.axisTop().scale(y);
-      x.domain(d3.extent(this.data, (d, i) => i));
-      y.domain([0, d3.max(this.data, (d) => d)]);
-      return d3
-        .line()
-        .x((d, i) => x(i))
-        .y((d) => y(d));
-    },
-    line() {
-      return this.path(this.data);
-    },
     viewBox() {
       return `0 0 ${this.width} ${this.height}`;
     },
+  },
+  methods: {
+    getScales() {
+      // Create x scale
+      const xData = d3
+        .scaleLinear()
+        .domain(d3.extent(this.data, (d, i) => i))
+        .range(this.rangeX);
+
+      // Create y scale
+      const yData = d3
+        .scaleLinear()
+        .domain([0, d3.max(this.data, (d) => d)])
+        .range(this.rangeY);
+
+      // Scale x on left axis
+      d3.axisBottom().scale(xData);
+
+      // Scale y on top axis
+      d3.axisLeft().scale(yData);
+
+      // Create x axis
+      const xAxis = d3.axisBottom().scale(xData);
+      d3.select(this.$refs.xAxis).call(xAxis);
+
+      // Create y axis
+      const yAxis = d3.axisLeft().scale(yData);
+      d3.select(this.$refs.yAxis).call(yAxis);
+
+      return {
+        xData,
+        yData,
+        xAxis,
+        yAxis,
+      };
+    },
+    calculatePath() {
+      const scale = this.getScales();
+      const path = d3
+        .line()
+        .x((d, i) => scale.xData(i))
+        .y((d) => scale.yData(d));
+      // .curve(d3.curveCardinal)
+
+      this.line = path(this.data);
+    },
+  },
+  mounted() {
+    this.calculatePath();
   },
 });
 </script>
